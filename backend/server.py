53075d37e6764dae6d713fa5bd85af6b4009bddb
Alexandros Dorodoulis <aldor94@gmail.com>
2018-11-28 15:22:00 +0100

    feat: Add api server

--- backend/server.py
+++ backend/server.py
@@ -10,6 +10,7 @@ from timeit import default_timer as timer
 import click
 import gensim
 import requests
+from aiohttp import web
 from gensim.models import KeyedVectors, Word2Vec
 from sqlalchemy import func
 
@@ -43,13 +44,13 @@ class API(object):
             for word in self.session.query(WordMoviesModel).all()
         ]
 
-    async def get_synonyms(self, inputArgs):
-        synonymList = [list() for i in inputArgs]
+    async def get_synonyms(self, input_args):
+        synonym_list = [list() for i in input_args]
 
         # grab synonyms from API
-        for position in range(len(inputArgs)):
+        for position in range(len(input_args)):
             r = requests.get(
-                f"https://od-api.oxforddictionaries.com:443/api/v1/entries/en/{inputArgs[position]}/synonyms",
+                f"https://od-api.oxforddictionaries.com:443/api/v1/entries/en/{input_args[position]}/synonyms",
                 headers={"app_id": self.APP_ID, "app_key": self.APP_KEY},
             )
             if r.ok:
@@ -60,23 +61,25 @@ class API(object):
                             for v in k["senses"]:
                                 for w in v["synonyms"]:
                                     if len(w["text"].split(" ")) == 1:
-                                        synonymList[position].append(w["text"])
+                                        synonym_list[position].append(w["text"])
 
         # load the desired model
         cbow = KeyedVectors.load_word2vec_format("cbow.bin", binary=False)
 
         # find synonyms from embedded model
-        for i in range(len(synonymList)):
-            for word in cbow.most_similar(inputArgs[i]):
+        for i in range(len(synonym_list)):
+            for word in cbow.most_similar(input_args[i]):
                 if len(word[0].split(" ")) == 1:
-                    synonymList[i].append(word[0])
-        word_list = [word for group in synonymList for word in group] + inputArgs
-        word_list = [word for word in word_list if self.bloom_filter.classify(word)]
-        return await self.scoring(synonymList, set(word_list), inputArgs)
+                    synonym_list[i].append(word[0])
+        word_list = [word for group in synonym_list for word in group]
+        word_list = set(
+            [
+                word
+                for word in word_list + input_args
+                if self.bloom_filter.classify(word)
+            ]
+        )
 
-    async def scoring(self, synonymList, word_list, original_args):
-
-        # make dictionary from the sql call
         word_dict = {}
         for entry in (
             self.session.query(WordMoviesModel)
@@ -87,8 +90,8 @@ class API(object):
 
         # loop through the dictionary and sum the movies
         movie_scores = {}
-        for group in range(len(synonymList)):
-            for word in synonymList[group]:
+        for group in range(len(synonym_list)):
+            for word in synonym_list[group]:
                 movies = word_dict.get(word, False)
                 if not movies:
                     continue
@@ -99,7 +102,7 @@ class API(object):
 
         for movie in movie_scores:
             original_syn_count = 0
-            for word in original_args:
+            for word in input_args:
                 if movie in word_dict[word]:
                     original_syn_count += 1
             movie_scores[movie] = (
@@ -109,30 +112,53 @@ class API(object):
                 + 5 * original_syn_count
             )
 
-        print("Based on your inputs, we recommend the following:", "\n")
         movies = sorted(movie_scores, key=lambda k: movie_scores[k], reverse=True)[:10]
         q = self.session.query(MovieModel).filter(MovieModel.id.in_(movies))
         movie_map = {movie.id: movie.title for movie in q}
-        [print(movie_map[int(id)]) for id in movies]
+        return [movie_map[int(id)] for id in movies]
+
+    async def handle_command_line_execution(self, words):
+        res = await self.get_synonyms(words)
+        print("Based on your inputs, we recommend the following:", "\n")
+        [print(movie) for movie in res]
+
+    async def handle_request(self, request):
+        try:
+            words = request.rel_url.query["words"]
+            return web.json_response(
+                {"results": await self.get_synonyms(words.split(","))}
+            )
+        except KeyError:
+            return web.json_response(
+                {"error": "Please provide some tags to be used in the movie selection"},
+                status=400,
+            )
 
 
 @click.command()
 @click.option(
     "--words",
     is_flag=False,
-    default=",".join(inputArgs),
     show_default=True,
     type=click.STRING,
     metavar="<words>",
     help="Sets the words that you want to search",
 )
 def main(words):
-    words = [arg.strip() for arg in words.split(",")]
     api = API()
-    # start = timer()
-    asyncio.get_event_loop().run_until_complete(api.get_synonyms(words))
-    # end = timer()
-    # print(end - start)
+    if words:
+        words = [arg.strip() for arg in words.split(",")]
+        # start = timer()
+        asyncio.get_event_loop().run_until_complete(
+            api.handle_command_line_execution(words)
+        )
+        # end = timer()
+        # print(end - start)
+    else:
+        app = web.Application()
+        app.add_routes([web.get("/", api.handle_request)])
+
+        web.run_app(app)
 
 
 if __name__ == "__main__":
