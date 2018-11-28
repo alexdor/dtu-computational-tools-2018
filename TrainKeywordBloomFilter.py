d7e55858c532cc489fd61188000e4ded8d264624
Barnabas Homola <homolabarnabas@gmail.com>
2018-11-28 11:28:14 +0100

    feat: Bloomfilter, Bitvector and BloomfilterTrainer added

---
+++ TrainKeywordBloomFilter.py
@@ -0,0 +1,13 @@
+from KeyWordBloomFilter import KeyWordBloomFilter
+import sqlite3
+
+conn = sqlite3.connect('backend/parser.sqlite3')
+c = conn.cursor()
+
+c.execute('Select word from word_movies')
+wordlist = [word[0] for word in c.fetchall()]
+bloomFilter = KeyWordBloomFilter(p=0.0001,n=len(wordlist))
+for word in wordlist:
+	bloomFilter.train(word)
+
+bloomFilter.write_to_file('BloomFilterBitVector')
